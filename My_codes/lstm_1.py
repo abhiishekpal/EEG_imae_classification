bb0b4d654d13b17c3b52dcefb8ea0f1a9e40853e
Abhishek Pal <knot15a@gmail.com>
2018-07-17 01:58:55 +0530

    lstm1.py is working

--- My_codes/lstm_1.py
+++ My_codes/lstm_1.py
@@ -7,9 +7,6 @@ import sys
 import os
 
 
-tf.reset_default_graph()
-Y_ = tf.placeholder(tf.float32, shape = (None, 40))
-X_ = tf.placeholder(tf.float32, shape = (None, 200, 128))
 total = 6000
 
 
@@ -22,7 +19,7 @@ def parse_args():
 
     parser.add_argument('--logs_path', type=str, default='./logs', help='File path of logs for tensorboard')
 
-    parser.add_argument('--n_hidden', type=int, default=256, help='Number of hidden units in a single LSTM   cell')
+    parser.add_argument('--n_hidden', type=int, default=512, help='Number of hidden units in a single LSTM   cell')
 
     parser.add_argument('--n_input', type=int, default=200, help='Number of input words')
 
@@ -40,6 +37,12 @@ def parse_args():
     return check_args(parser.parse_args())
 
 def check_args(args):
+
+
+    try:
+        f = open(args.text_path,'r')
+    except Exception:
+        print("no such file found to be read")
 
     try:
         os.mkdir(args.logs_path)
@@ -94,11 +97,13 @@ def check_args(args):
     return args
 
 
+tf.reset_default_graph()
 
 
-def get_data(ind, args):
+def get_data(ind):
     label_path = '/home/knot/Documents/MTP/data/eeg_label.npy'
     input_path = '/home/knot/Documents/MTP/data/input_eeg.npy'
+
     n_input = args.n_input
     batch_size  = args.batch_size
     n_classes = args.n_classes
@@ -107,13 +112,13 @@ def get_data(ind, args):
     mat_input = np.load(input_path)
     mat_label = np.load(label_path)
 
-    total = mat_input.shape[0]
-    X = np.zeros((batch_size, n_input, 128))
+    total = mat_inout.shape[0]
+    X = np.zeros((batch_size, 128, n_input))
     Y = np.zeros((batch_size, n_classes))
 
 
-    for i in range(batch_size   ):
-        temp = mat_input[ind+i,:,:]
+    for i in range(8):
+        temp = mat_input[ind+i,:,:].T
         X[i,:,:] = temp
         temp = mat_label[ind+i,:,:]
         temp = np.squeeze(temp, axis = 0)
@@ -130,17 +135,17 @@ def rnn(inp_, args):
     n_input = args.n_input
     n_hidden = args.n_hidden
     n_stacks = args.n_stacks
-    n_classes = args.n_classes
 
-    inp_ = tf.unstack(inp_ ,n_input, 1)
 
+    inp_ = tf.reshape(inp_, [-1, n_input])
+    inp_ = tf.split(inp_, n_input, 1)
 
     rnn_cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(n_hidden) for _ in range(n_stacks)])
     outputs, states = tf.contrib.rnn.static_rnn(rnn_cell, inp_, dtype=tf.float32)
 
     dense1 = tf.layers.dense(inputs = outputs[-1], units = 512, activation = tf.nn.relu, name = "dense1")
-    logit = tf.layers.dense(inputs = dense1, units = n_classes)
-    out = tf.nn.softmax(logit, name="softmax_output")
+    logits = tf.layers.dense(inputs = dense1, units = n_classes)
+    out = tf.nn.softmax(logits, name="softmax_output")
 
     return out
 
@@ -156,14 +161,14 @@ def main(args):
     batch_size = args.batch_size
     check_path = args.check_path
 
+    x = tf.placeholder(tf.float32, shape = (None, n_input, 128))
+    y = tf.placeholder(tf.float32, shape = (None, n_classes))
 
-    pred = rnn(X_, args)
-    # print(eval("pred"))
-    # print(eval("Y_"))
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = pred, labels=Y_))
+    pred = rnn(x, args)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
     optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(cost)
 
-    correct = tf.equal(tf.argmax(pred, 1), tf.argmax(Y_, 1))
+    correct = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
     tf.summary.scalar("cost", cost)
@@ -182,14 +187,15 @@ def main(args):
         for epoch in range(epochs):
             epoch_loss = 0
             avg_acc = 0
-            for ind in progressbar.progressbar(range(0,total,batch_size)):
+            for ind in progressbar.progressbar(0,range(total),batch_size):
                 batchx = np.zeros((batch_size, n_input, 128))
                 batchy = np.zeros((batch_size, n_classes))
 
                 batchx, batchy = get_data(ind, args)
                 batchx = np.array(batchx)
                 batchy = np.array(batchy)
-                _, loss, onehot_pred, acc, summary = sess.run([optimizer, cost, pred, accuracy, summary_op], feed_dict={X_: batchx, Y_: batchy})
+
+                _, loss, onehot_pred, acc, summary = sess.run([optimizer, cost, pred, accuracy, summary_op], feed_dict={x: batchx, y: batchy})
                 writer.add_summary(summary, pt)
                 pt+=1
                 avg_acc +=  acc
